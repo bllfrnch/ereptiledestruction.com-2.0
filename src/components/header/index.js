cf9600239aeafbb0bab93e1d2f105f884e4ad95c
Bill French <bllfrnch@gmail.com>
2019-08-07 23:18:14 -0400

    Added header image

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -2,27 +2,21 @@ import { Link } from 'gatsby';
 import PropTypes from 'prop-types';
 import React from 'react';
 import styles from './header.module.scss';
+import ereptileLogo from '../../images/ed-logo.png';
+import ereptileLogoMobile from '../../images/ed-logo-mobile.png';
 
 const Header = ({ siteTitle }) => (
   <header
     className={styles.header}
   >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
+    <div>
+      <h1>
+        <Link to="/">
+          <picture>
+            <source srcset={ereptileLogo} media="(min-width: 601px)" alt="EREPTILE DESTRUCTION" aria-label="Ereptile Destruction" />
+            <source srcset={ereptileLogoMobile} media="(max-width: 600px)" alt="EREPTILE DESTRUCTION" aria-label="Ereptile Destruction" />
+            <img src={ereptileLogo} alt="EREPTILE DESTRUCTION" aria-label="Ereptile Destruction" />
+          </picture>
         </Link>
       </h1>
     </div>
