5dd47506ed122afdea66f803ede777cf41996395
Bill French <bllfrnch@gmail.com>
2019-08-10 00:20:11 -0400

    Added header styling for sections

--- src/components/section/index.js
+++ src/components/section/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link, useStaticQuery, graphql } from 'gatsby';
+import cx from 'classnames';
 import styles from './section.module.scss';
 
 const Section = props => {
@@ -7,7 +8,9 @@ const Section = props => {
   const { headline, id } = data;
   return (
     <section id={id} className={styles.section}>
-      <h2>{headline}</h2>
+      <h2 className={styles.sectionHeadline}>
+        <span className={cx(styles.icon, styles[id])}>{headline}</span>
+      </h2>
       <div className={styles.sectionContentWrapper}>{children}</div>
     </section>
   );
